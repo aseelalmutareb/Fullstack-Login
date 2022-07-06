672ec22aac775db2acb26a1db9565a2cbfe2c5d1
Aseel <aseel.almutareb@gmail.com>
2022-07-06 17:49:38 +0200

    fetchers

---
+++ frontend/src/fetchers/index.js
@@ -0,0 +1,33 @@
+export const registerUser = async (userData) => {
+    const response = await fetch(`${URL}/register`, {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+  
+    return data;
+  };
+  
+  export const loginUser = async (userData) => {
+    const response = await fetch(`${URL}/login`, {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+  
+    return data;
+  };
+  
+  export const updateUser = async (userData) => {
+    const response = await fetch(`${URL}/update`, {
+      method: "PUT",
+      body: JSON.stringify(userData),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+  
+    return data;
+  };
+  
